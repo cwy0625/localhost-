5526dee6e6c570833da0f61d2911b10c8cc06979
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-05-08 20:36:26 +0900

    ##1 백엔드 게시판 서비스 구현 :: V1

---
+++ localhost/src/main/java/bssm/spring/board/repository/BoardRepository.java
@@ -0,0 +1,11 @@
+package bssm.spring.board.repository;
+
+import bssm.spring.board.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitleContaining(String keyword);
+}
